aa4e9abf5141667246307dc97765e90b3a604ae3
Andy Zimolzak, Pi 3 model B <andyzimolzak@gmail.com>
2023-02-12 09:04:43 -0600

    print little dot, catch addstr exceptions?

--- plot.py
+++ plot.py
@@ -1,3 +1,5 @@
+# dump1090-mutability 2> /dev/null | python plot.py
+
 from time import sleep
 import sys
 import curses
@@ -11,7 +13,7 @@ def long2col(longit, cols=C, min_long=-96, max_long=-95.1):
     m = cols / (max_long - min_long)
     return int(round(m * (longit - min_long)))
 
-def lat2row(lat, rows=R, min_lat=29.6, max_lat=29.9):
+def lat2row(lat, rows=R, min_lat=29.6, max_lat=30.0):
     m = -1 * rows / (max_lat - min_lat)
     return int(round(m * (lat - max_lat)))
 
@@ -20,10 +22,19 @@ def main(stdscr):
     lon = REF_LON
     myrow = lat2row(lat)
     mycol = long2col(lon)
+    last_icao = ''
+    last_ident = ''
+    icao_ident = {}
+    curses.initscr()  # not sure if needed
     stdscr.addstr(myrow, mycol, "H")
     stdscr.refresh()
 
     for line in sys.stdin:
+        if 'ICAO Addr' in line:
+            last_icao = line[17:]
+        if 'Ident:' in line:
+            last_ident = line[17:]
+            icao_ident[last_icao] = last_ident
         if 'latitude' in line and '.' in line:
             lat = float(line[17:26])
             continue
@@ -31,8 +42,16 @@ def main(stdscr):
             lon = float(line[17:27])
             myrow = lat2row(lat)
             mycol = long2col(lon)
-            stdscr.addstr(myrow, mycol, ".")
-            stdscr.refresh()
+            try:
+                id_str = '.' + icao_ident[last_icao]
+            except KeyError:  # When ICAO has no Ident yet
+                id_str = '.'
+            #if 0 <= myrow <= R and 0 <= mycol <= C:
+            try:
+                stdscr.addstr(myrow, mycol, id_str)
+                stdscr.refresh()
+            except Exception:
+                pass
     sleep(10)
 
 curses.wrapper(main)
